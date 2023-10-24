5b1a418a1ce63e27a077b2e92fafde70263e0a1c
Hamza Oumeziane <116696843+Omz123@users.noreply.github.com>
2023-10-24 10:55:41 -0400

    Faire les classes ElementGTD + Ajout de la classe de Date

---
+++ ClassesAffaire/Date.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAffaire
+{
+    internal class Date
+    {
+        public int Annee { get; set; }
+        public int Mois { get; set; }
+        public int Jour { get; set; }
+
+        public Date(int annee, int mois, int jour)
+        {
+            Annee = annee;
+            Mois = mois;
+            Jour = jour;
+        }
+
+        public string ToString()
+        {
+            return $"{Annee}-{Mois}-{Jour}";
+        }
+
+    }
+}
--- ClassesAffaire/ElementGTD.cs
+++ ClassesAffaire/ElementGTD.cs
@@ -1,7 +1,51 @@
-﻿namespace GTD
+﻿using ClassesAffaire;
+
+namespace GTD
 {
     public class ElementGTD
     {
+        public string Nom { get; set; }
+        public string Description { get; set; }
+        public Date DateRappel { get; set; }
+        public Statut Statut { get; set; }
 
+        public ElementGTD(string nom, Statut statut, string description, Date dateRappel) 
+        { 
+            Nom = nom;
+            Statut = statut;
+            Description = description;
+            DateRappel = dateRappel;
+            
+        }
+
+        public ElementGTD(string nom, Statut statut)
+        {
+            Nom = nom;
+            Statut = statut;
+        }
+
+        public ElementGTD(string nom, Statut statut, string description)
+        {
+            Nom = nom;
+            Statut = statut;
+            Description = description;
+        }
+
+        public ElementGTD(string nom, Statut statut, Date dateRappel)
+        {
+            Nom = nom;
+            Statut = statut;
+            DateRappel = dateRappel;
+        }
+
+    }
+
+
+    public enum Statut
+    {
+        Entree,
+        Action,
+        Suivi,
+        Archive
     }
 }
